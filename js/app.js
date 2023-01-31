26bb58264fc7dd7128288808e143ddb0d87291fe
Francesco Punta <puntafrancesco@gmail.com>
2023-01-31 17:15:08 +0100

    Console log

--- js/app.js
+++ js/app.js
@@ -4,10 +4,12 @@ let firstName, km, anni
 
 // 1. chiedere il nome
 firstName = prompt('Ciao! come ti chiami?')
-
+console.log(firstName)
 
 //2. Km da percorrere
 km = prompt('Bel nome! Quanti km vuoi percorrere?')
+console.log(km)
 
 //3. Anni dell'utente
 anni = prompt('Quanti anni hai?')
+console.log(anni)
