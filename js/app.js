580f0a27a84a2a1387ee3b2511698f1cabd02e6a
Francesco Punta <puntafrancesco@gmail.com>
2023-01-31 18:02:31 +0100

    Totale

--- js/app.js
+++ js/app.js
@@ -16,20 +16,31 @@ anni = prompt('Quanti anni hai?')
 anni = parseFloat(anni)
 console.log(anni)
 
+//4. Prezzo al km
+const base = 0.21 
+console.log('Il prezzo al km è:', base)
+
+//5. Definisco il prezzo per utente standard
+const totale = (km * base)
+console.log('Il totale è:', totale)
+
+
 
 // Calcolo sconto per utente
 
 if (anni < 19) {
-    const scontoUnder = 20
+    const scontoUnder = 0.20
     console.log('sconto under')
 }
  else  {
     if (anni > 65){
-        const scontoOver = 40
+        const scontoOver = 0.40
         console.log('sconto over')
     }
  }
 
  //Calcolo prezzo
+
+ 
 
 
