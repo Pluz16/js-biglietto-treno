c0e70e8d30ca01a40b3563d4f43d8156af570d33
Francesco Punta <puntafrancesco@gmail.com>
2023-01-31 17:13:52 +0100

    km e anni

--- js/app.js
+++ js/app.js
@@ -4,3 +4,10 @@ let firstName, km, anni
 
 // 1. chiedere il nome
 firstName = prompt('Ciao! come ti chiami?')
+
+
+//2. Km da percorrere
+km = prompt('Bel nome! Quanti km vuoi percorrere?')
+
+//3. Anni dell'utente
+anni = prompt('Quanti anni hai?')
