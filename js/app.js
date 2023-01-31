78298ba02cb53c410cdfb2b1470a4f2e4f349a03
Francesco Punta <puntafrancesco@gmail.com>
2023-01-31 17:52:53 +0100

    Sconto

--- js/app.js
+++ js/app.js
@@ -8,8 +8,28 @@ console.log(firstName)
 
 //2. Km da percorrere
 km = prompt('Bel nome! Quanti km vuoi percorrere?')
+km = parseFloat(km)
 console.log(km)
 
 //3. Anni dell'utente
 anni = prompt('Quanti anni hai?')
+anni = parseFloat(anni)
 console.log(anni)
+
+
+// Calcolo sconto per utente
+
+if (anni < 19) {
+    const scontoUnder = 20
+    console.log('sconto under')
+}
+ else  {
+    if (anni > 65){
+        const scontoOver = 40
+        console.log('sconto over')
+    }
+ }
+
+ //Calcolo prezzo
+
+
